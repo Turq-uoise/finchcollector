3e29d7b4b8a3dad96dea8a1115e5640eae281516
Turq-uoise <Turquoise_2@outlook.com>
2023-06-14 12:47:01 +0100

    Added Finch List and About page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+finches = [
+    {
+        "species": "Zebra Finch",
+        "color": "Gray",
+        "beak_length": 1.5,
+        "wing_span": 9.8,
+        "weight": 15.2,
+    },
+    {
+        "species": "Gouldian Finch",
+        "color": "Multicolored",
+        "beak_length": 1.2,
+        "wing_span": 10.5,
+        "weight": 12.7,
+    },
+    {
+        "species": "Society Finch",
+        "color": "White",
+        "beak_length": 1.4,
+        "wing_span": 8.9,
+        "weight": 14.1,
+    }
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
